1282c8f088bc0facbe8c0e2af240da7b5b39c709
Hian <anhkhung006@gmail.com>
2025-11-02 15:37:23 +0700

    add rabbitmq run with aspire host

--- Backend/ELibrary_PRN232.AppHost/Program.cs
+++ Backend/ELibrary_PRN232.AppHost/Program.cs
@@ -1,5 +1,19 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var rabbit = builder
+    .AddContainer("rabbitmq", image: "rabbitmq", tag: "3.13-management")
+    .WithEndpoint(
+        name: "amqp",
+        port: 5672,
+        targetPort: 5672,
+        scheme: "amqp")
+    .WithHttpEndpoint(
+        name: "mgmt",
+        port: 15672,
+        targetPort: 15672)
+    .WithEnvironment("RABBITMQ_DEFAULT_USER", "guest")
+    .WithEnvironment("RABBITMQ_DEFAULT_PASS", "guest");
+
 builder.AddProject<Projects.Elib_Storage_Service>("elib-storage-service");
 
 builder.AddProject<Projects.Elib_Catalog_Service>("elib-catalog-service");
